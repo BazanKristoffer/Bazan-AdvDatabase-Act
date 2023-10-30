ad222508be268e307823f912f797956f25eb3cb7
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-30 22:49:16 +0800

    Update mysqlconfig_connection.php

--- webdevproject/dbConnection/mysqlconfig_connection.php
+++ webdevproject/dbConnection/mysqlconfig_connection.php
@@ -4,5 +4,6 @@
     DEFINE('DB_HOST','localhost');
     DEFINE('DB_NAME','db');
 
-    
+    $dbc = @mysqli_connection(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+    OR dies('Could not connect to MySQL: ' . mysql_connect_error());
 ?>
