bb242284dfa700f500a84b948ca8b8685e2222a4
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 10:39:36 +0800

    Update index.php

--- webdevproject/index.php
+++ webdevproject/index.php
@@ -2,3 +2,9 @@
     include_once("dbConnection/mysqlconfig_connection.php");
     include_once("functionsfetch.php");
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sample CRUD</title>
+    </head>
+</html>
