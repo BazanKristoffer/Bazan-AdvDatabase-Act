b8d7df7f483cae1ac738bbb8b9b4b79d1ac603d0
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 12:23:02 +0800

    Update addform.php

---
+++ webdevproject/forms/addform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add Data</title>
+    </head>
+    <body>
+        <h1>Add subject</h1>
+        <a href="../index.php">Home</a>
+        <br/><br/>
+
+        <form action="../functions/add.php" method="POST" name="form1">
+            <table></table>
+        </form>
+    </body>
+</html>
