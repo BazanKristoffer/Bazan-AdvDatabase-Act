d3fec30d0f9f5c1b7cff11066158fd345ce96d38
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 12:25:52 +0800

    Update addform.php

--- webdevproject/forms/addform.php
+++ webdevproject/forms/addform.php
@@ -14,6 +14,16 @@
                     <td>Subject Code</td>
                     <td><input type="text" name="code"></td>
                 </tr>
+                
+                <tr>
+                    <td>Subject Name</td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="Submit" value="Add"></td>
+                </tr>
             </table>
         </form>
     </body>
