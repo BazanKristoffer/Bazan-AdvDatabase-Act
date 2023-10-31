f50f085630e09a087fcd672934a33e73956f42aa
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 10:45:52 +0800

    Update index.php

--- webdevproject/index.php
+++ webdevproject/index.php
@@ -12,7 +12,10 @@
         <a href="forms/addform.php">Add Subject</a><br/><br/>
         <table width='100%' border= 1>
             <tr bgcolor='#CCCCCC'>
-
+                <td>ID</td>
+                <td>Subject Code</td>
+                <td>Subject Name</td>
+                <td>Action</td>
             </tr>
 
         </table>
