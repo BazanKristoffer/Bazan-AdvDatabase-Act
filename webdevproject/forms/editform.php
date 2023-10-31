cede880bf2a8b4512a7c5026988ddf77f808dc7e
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 12:38:31 +0800

    Update editform.php

--- webdevproject/forms/editform.php
+++ webdevproject/forms/editform.php
@@ -4,5 +4,14 @@
         $id = $_POST['id'];
         $code = $_POST['code'];
         $name = $_POST['name'];
+
+        if(empty($code)|| empty($name)) {
+            if(empty($code)) {
+                echo "<font color ='red'>Subject Code field is empty.</font><br/>";
+            }
+            if(empty($name)) {
+                echo "<font color ='red'>Subject Name field is empty.</font><br/>";
+            }
+        }
     }
 ?>
