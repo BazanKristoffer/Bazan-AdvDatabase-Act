1d7aaeca8555604bf3914dbafc32ba74f1dcbcd7
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 11:07:05 +0800

    Update add.php

--- webdevproject/functions/add.php
+++ webdevproject/functions/add.php
@@ -10,6 +10,16 @@
         if(isset($_POST['Submit'])) {
             $code = $_POST['code'];
             $name = $_POST['name'];
+
+            if(empty($code) || empty($name)) {
+                if(empty($code)) {
+                    echo "<font color='red>'Subject Code field is empty.</font><br/>";
+                }
+                if(empty($name)) {
+                    echo "<font color='red'>Subject Name field is empty.</font><br/> ";
+                }
+                echo"<br/><a href='javascript:self.history.back();'>Go Back<a/>";
+            }
         }
         ?>
     </body>
