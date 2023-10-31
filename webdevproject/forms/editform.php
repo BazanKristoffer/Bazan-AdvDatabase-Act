c769460cdf3d8e11addf85bbc0942da8e8cb5e84
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-10-31 12:29:03 +0800

    Update editform.php

---
+++ webdevproject/forms/editform.php
@@ -0,0 +1,8 @@
+<?php
+    include_once("../dbConnection/mysqlconfig_connection.php");
+    if(isset($_POST['update'])) {
+        $id = $_POST['id'];
+        $code = $_POST['code'];
+        $name = $_POST['name'];
+    }
+?>
