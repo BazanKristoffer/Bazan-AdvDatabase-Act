69a5c4057fb6892408253956a841c6b694f80e33
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-11-27 18:47:10 +0800

    Update index.php

--- webdevproject/index.php
+++ webdevproject/index.php
@@ -15,6 +15,9 @@
                 <td>ID</td>
                 <td>Subject Code</td>
                 <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
             <?php
