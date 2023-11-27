d5968c40876841bb02fc0cd3a11ae7be48ca7cce
BazanKristoffer <kristofferbazan21edu@gmail.com>
2023-11-27 18:58:21 +0800

    Update addsyllabusform.php

---
+++ webdevproject/forms/addsyllabusform.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add Syllabus</title>
+    </head>
+    <body>
+        <?php
+            include_once("../dbConnection/mysqlconfig_connection.php");
+
+            if(isset($_POST['Submit'])) {
+                $code = $_POST['code'];
+                $name = $_POST['author'];
+                $subject = $_POST['subject'];
+
+                if(empty($code) || empty($author)) {
+                    if(empty($code)) {
+                        echo "<font color='red>'Syllabus Code field is empty.</font><br/>";
+                    }
+                    if(empty($name)) {
+                        echo "<font color='red'>Syallabus Author field is empty.</font><br/> ";
+                    }
+                    echo"<br/><a href='javascript:self.history.back();'>Go Back<a/>";
+                }    
+                else {
+                    $result = mysqli_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name) VALUES('$code','$name')");
+                    echo "<font color='green'>Data added sucessfully.";
+                    echo "<br/><a href='../index.php'>View Result</a>";
+                }
+            }
+        ?>
+    </body>
+</html>
